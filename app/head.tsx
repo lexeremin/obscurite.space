05f29b4fa98ddf71ce48bef60d5f50f81cac987b
Alexander Eremin <Seexub@gmail.com>
2023-02-03 13:25:12 +0300

    added an existed project files to the repo

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+      <>
+        <title>Obscure Space</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"  />
+        <meta name="description" content="The space to grasp the unknown." />
+        <link rel="icon" href="/obscure.ico" />
+      </>
+    );
+  }
